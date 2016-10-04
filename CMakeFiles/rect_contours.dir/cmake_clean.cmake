d5eaaa7960f973c1662c25d13c6a1bb6e3eda10c
Helldogify <mahipaul.tak@gmail.com>
2016-10-04 09:12:34 -0600

    trying to find measurements
    currently only able to detect using contours

---
+++ CMakeFiles/rect_contours.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rect_contours.dir/rect_contours.cpp.o"
+  "rect_contours.pdb"
+  "rect_contours"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rect_contours.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
